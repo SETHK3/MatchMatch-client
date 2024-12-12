b5bec65ecbeab2c94cc18cf66e9c91bd3c75c818
sethk3 <threegartz@gmail.com>
2024-12-11 20:44:27 -0700

    initial html and scss styles

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,18 +1,69 @@
 <template>
-  <h1>MatchMatch</h1>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./../assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="home">
+    <div class="navbar-wrapper">
+      <div class="navbar-logo">
+        <!-- <img src="" alt="logo" class="logo" /> -->
+        <h1>MatchMatch</h1>
+      </div>
+      <div class="nav-links">
+        <a href="#">Home</a>
+        <a href="#">Tabs</a>
+        <a href="#">Tabs</a>
+        <a href="#">Tabs</a>
+      </div>
+      <button class="account">Create Account</button>
+    </div>
+
+    <div class="main-content">
+      <div class="logo-toggle">
+        <a href="https://vite.dev" target="_blank">
+          <img src="/vite.svg" class="logo" alt="Vite logo" />
+        </a>
+        <a href="https://vuejs.org/" target="_blank">
+          <img src="./../assets/vue.svg" class="logo vue" alt="Vue logo" />
+        </a>
+      </div>
+    </div>
   </div>
 </template>
 
-<script></script>
+<script>
+export default {
+  name: "Home",
+  setup() {
+    return {};
+  },
+};
+</script>
 
-<style scoped>
+<style lang="scss" scoped>
+.home {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-family: Lexend, sans-serif;
+  color: #272d41;
+  width: 100%;
+  box-sizing: border-box;
+}
+
+.navbar-wrapper {
+  position: fixed;
+  width: 100%;
+  z-index: 1;
+  background-color: white;
+
+  .navbar-logo {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    padding: 1rem 1rem;
+    background-color: white;
+    border-bottom: 1px solid gray;
+    box-sizing: border-box;
+  }
+}
 .logo {
   height: 6em;
   padding: 1.5em;
